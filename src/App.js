b8384308b01c38b74dd7747c8d43207559e8597e
kjh-busan <picsmash7@gmail.com>
2023-03-31 14:23:44 +0900

    feat: create useHooks

--- src/App.js
+++ src/App.js
@@ -8,32 +8,32 @@ function App() {
   const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-http-3d9ed-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
-      );
+  // const fetchTasks = async (taskText) => {
+  //   setIsLoading(true);
+  //   setError(null);
+  //   try {
+  //     const response = await fetch(
+  //       "https://react-http-3d9ed-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
+  //     );
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("Request failed!");
+  //     }
 
-      const data = await response.json();
+  //     const data = await response.json();
 
-      const loadedTasks = [];
+  //     const loadedTasks = [];
 
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
+  //     for (const taskKey in data) {
+  //       loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+  //     }
 
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+  //     setTasks(loadedTasks);
+  //   } catch (err) {
+  //     setError(err.message || "Something went wrong!");
+  //   }
+  //   setIsLoading(false);
+  // };
 
   useEffect(() => {
     fetchTasks();
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,3 +1,45 @@
-const useHttp = () => {};
+import { useState } from "react";
+
+const useHttp = (requestConfig, applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    // "https://react-http-3d9ed-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json"
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method,
+        headers: requestConfig.headers,
+        body: JSON.stringify(requestConfig.body),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      const loadedTasks = [];
+
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+
+      // setTasks(loadedTasks);
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading, // === isLoading: isLoading,
+    error, // === error: error,
+    sendRequest, // === sendRequest: sendRequest,
+  };
+};
 
 export default useHttp;
